63dcdb207a6c5b6547b5797500f34fcb744ed647
Cecconi Baptiste <baptiste.cecconi@obspm.fr>
2026-09-09 14:03:05 +0200

    Refactor

--- data_citation_reporter/openaire.py
+++ data_citation_reporter/openaire.py
@@ -28,12 +28,8 @@ def get_scholexplorer(pid, api_url=SCHOLEXPLORER_V1_URL):
         citation_set = set()
         for item in data:
             for identifiers in item["target"]["identifiers"]:
-                citation_set.add(
-                    (identifiers["identifier"], identifiers["schema"])
-                )
-        print(
-            f"citing: {', '.join([f'{cite_item[1]}:{cite_item[0]}' for cite_item in citation_set])}"
-        )
+                citation_set.add((identifiers["identifier"], identifiers["schema"]))
+        print(f"citing: {', '.join([f'{cite_item[1]}:{cite_item[0]}' for cite_item in citation_set])}")
         src_pid = URIRefDoi(doi.lower())
         for identifier, schema in citation_set:
             if schema == "doi":
@@ -43,13 +39,9 @@ def get_scholexplorer(pid, api_url=SCHOLEXPLORER_V1_URL):
             elif schema == "handle":
                 bib_pid = URIRef(f"http://hdl.handle.net/{identifier}")
             elif schema == "pmc":
-                bib_pid = URIRef(
-                    f"https://www.ncbi.nlm.nih.gov/pmc/articles/{identifier}/"
-                )
+                bib_pid = URIRef(f"https://www.ncbi.nlm.nih.gov/pmc/articles/{identifier}/")
             elif schema == "pmid":
-                bib_pid = URIRef(
-                    f"https://pubmed.ncbi.nlm.nih.gov/{identifier}/"
-                )
+                bib_pid = URIRef(f"https://pubmed.ncbi.nlm.nih.gov/{identifier}/")
             else:
                 bib_pid = None
 
@@ -74,13 +66,15 @@ def get_scholexplorer(pid, api_url=SCHOLEXPLORER_V1_URL):
 
 # https://api.openaire.eu/graph/v1/researchProducts/links?targetPid=10.25935%2Fnhb2-wy29&page=0&pageSize=100
 def get_openaire_graph(pid, api_url=OPENAIREGRAPH_V1_URL):
+
     g = Graph()
+
     doi = str(pid).replace("https://doi.org/", "")
     access_url = f"{api_url}?targetPid={doi}&page=0&pageSize=100"
+
     data = get(access_url)
     if data is None:
         return g
-
     data = data["results"]
     print(f"{doi}: {len(data)}")
     if len(data) > 0:
@@ -92,15 +86,10 @@ def get_openaire_graph(pid, api_url=OPENAIREGRAPH_V1_URL):
                     source_pid = URIRefDoi(identifier["idUrl"])
                     break
             if source_pid is not None:
-                relation = OPENAIRE_SCHEMAS[item["relType"]["typeSchema"]][
-                    item["relType"]["name"]
-                ]
+                relation = OPENAIRE_SCHEMAS[item["relType"]["typeSchema"]][item["relType"]["name"]]
                 provenance = item["provenance"]
 
-                provenance = (
-                    "OpenAIRE Graph"
-                    + f" (via {", ".join([prov for prov in provenance])})"
-                )
+                provenance = "OpenAIRE Graph" + f" (via {", ".join([prov for prov in provenance])})"
 
                 # citation_set.add((source_pid, relation, provenance))
                 print(f"{source_pid} {relation} {doi} ({provenance})")
