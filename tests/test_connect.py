65495d72e610a159d7c90009a46347ce025fdb89
Cecconi Baptiste <baptiste.cecconi@obspm.fr>
2026-09-10 09:54:41 +0200

    Update tests using cache management

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -28,7 +28,7 @@ def test_get_success(mocker, test_url, test_headers):
     mock_get.return_value = mock_response
 
     # Calling the function
-    result = get(test_url, test_headers)
+    result = get(test_url, test_headers, use_cache=False)
 
     # Checking
     assert result == {"key": "value", "data": "test"}
@@ -43,7 +43,7 @@ def test_get_connection_error(mocker, test_url, test_headers):
     # Capture the print output
     mock_print = mocker.patch("builtins.print")
 
-    result = get(test_url, test_headers)
+    result = get(test_url, test_headers, use_cache=False)
 
     # Verify that the function returns None
     assert result is None
@@ -62,7 +62,7 @@ def test_get_http_error(mocker, test_url, test_headers):
 
     mock_print = mocker.patch("builtins.print")
 
-    result = get(test_url, test_headers)
+    result = get(test_url, test_headers, use_cache=False)
 
     # Verify that the function returns None
     assert result is None
@@ -82,7 +82,7 @@ def test_get_json_decode_error(mocker, test_url, test_headers):
 
     mock_print = mocker.patch("builtins.print")
 
-    result = get(test_url, test_headers)
+    result = get(test_url, test_headers, use_cache=False)
 
     # Verify that the function returns None
     assert result is None
@@ -99,7 +99,7 @@ def test_get_empty_response(mocker, test_url, test_headers):
     mock_response.raise_for_status.return_value = None
     mock_get.return_value = mock_response
 
-    result = get(test_url, test_headers)
+    result = get(test_url, test_headers, use_cache=False)
 
     assert result == {}
 
@@ -113,7 +113,7 @@ def test_get_large_response(mocker, test_url, test_headers):
     mock_response.raise_for_status.return_value = None
     mock_get.return_value = mock_response
 
-    result = get(test_url, test_headers)
+    result = get(test_url, test_headers, use_cache=False)
 
     assert result == large_data
     assert len(result["items"]) == 1000
@@ -127,6 +127,23 @@ def test_get_empty_header(mocker, test_url, test_headers):
     mock_response.raise_for_status.return_value = None
     mock_get.return_value = mock_response
 
-    result = get(test_url)
+    result = get(test_url, use_cache=False)
 
     assert result == {"key": "value", "data": "test"}
+
+
+def test_get_cache(mocker, test_url, test_headers):
+    """Success test - status 200 with valid JSON output"""
+    # Mock of the response
+    mock_get = mocker.patch("requests.get")
+    mock_response = mocker.Mock()
+    mock_response.json.return_value = {"key": "value", "data": "test"}
+    mock_response.raise_for_status.return_value = None
+    mock_get.return_value = mock_response
+
+    # Calling the function
+    result_no_cache = get(test_url, test_headers, use_cache=False)
+    result_cache = get(test_url, test_headers, use_cache=True)
+
+    # Checking
+    assert result_no_cache == result_cache
